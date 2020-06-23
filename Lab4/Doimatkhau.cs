21e79fa3989372e2da0a03eb5a2d4e2d6689ddda
SangLe <50876278+LeSang0169@users.noreply.github.com>
2020-06-23 23:22:30 +0700

    update password

--- Lab4/Doimatkhau.cs
+++ Lab4/Doimatkhau.cs
@@ -31,7 +31,28 @@ namespace Lab4
 
         private void btnCapnhap_Click(object sender, EventArgs e)
         {
-          
+            errorProvider1.SetError(txtDoimatkhau, "");
+            errorProvider1.SetError(txtNhaplaimatkhau, "");
+            if(txtDoimatkhau.Text.Length<8 || !txtDoimatkhau.Text.Any(char.IsDigit)||!txtDoimatkhau.Text.Any(char.IsLower)||!txtDoimatkhau.Text.Any(char.IsUpper))
+            {
+                errorProvider1.SetError(txtDoimatkhau, "Mật khẩu mới tối thiểu 8 ký tự, gồm chữ số," + "in hoa, in thường");
+                return;
+            }
+            if(txtDoimatkhau.Text != txtNhaplaimatkhau.Text)
+            {
+                errorProvider1.SetError(txtNhaplaimatkhau, "Mật khẩu nhập lại không đúng!!");
+                return;
+            }
+            NhanVien f = (NhanVien)this.MdiParent;
+            int count = XLBANG.Thuc_hien_lenh("Update NHANVIEN set Password ='" + txtDoimatkhau.Text + "'where MaNV ='" + f.maNV + "'");
+            if (count > 0)
+            {
+                MessageBox.Show("Cập nhật thành công");
+            }
+            else
+            {
+                MessageBox.Show("Mật khẩu không hợp lệ!!");
+            }
         }
     }
 }
